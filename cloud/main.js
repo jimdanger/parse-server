fbc0badb7e4fc6471cce0418d5554eede6e73c2e
Jim Wilson <jamesmattheywilson@gmail.com>
2016-06-13 09:46:45 -0400

    Updated file main.js
    Added another function to the cloudcode in main.js, just a simple function that is very similar to the hello-hi function that is in the parse example. This way, I can make sure cloud-code and git-updates are deploying to heroku and my parse backend.

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,8 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+
+Parse.Cloud.define('helloVersionTwo', function(req, res) {
+  res.success('Hi there, this is the new response');
+});
